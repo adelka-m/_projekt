3801729ee1ccb0f64baa7aeabaa371d4b11f9975
barbara-arual <58667619+barbara-arual@users.noreply.github.com>
2019-12-17 19:12:45 +0100

    Update script.py

--- script.py
+++ script.py
@@ -7,23 +7,29 @@ import matplotlib.pyplot as plt
 
 
 
+u0 = lambda x,y: np.sin(np.pi*x) + np.sin(np.pi*y)
+
 xs_help = np.linspace(0.1,1,10)
 xs = np.concatenate((-xs_help[::-1], np.array([0]), xs_help))
 
-Xs, Ys = np.meshgrid(xs,xs)      ### creating an equidistant grid for a 'symmetric' L shape
+Xs, Ys = np.meshgrid(xs,xs)
+
+Us = u0(Xs,Ys)
 for i in np.arange(len(xs)):
     for j in np.arange(len(xs)):
         if xs[i] > 0 and xs[j] > 0:
-            Xs[i][j] = 0
-            Ys[j][i] = 0
-
-"""
-#Visualizing the L
-Zs = np.ones((len(xs),len(xs)))
-for i in np.arange(len(xs)):
-    for j in np.arange(len(xs)):
-        if xs[i] > 0.01 and xs[j] > 0.01:
-            Zs[i,j] = 0
+            Us[i][j] = 0
+            Us[j][i] = 0                      ### BC: u = 0
         
-plt.scatter(Xs,Ys,c=Zs)     ### Is there a problem at (0,0) or is this just the Scatterplot marking the origin?
-"""
+        
+##Visualization        
+plt.scatter(Xs,Ys,c=Us)
+plt.colorbar()
+plt.show()
+
+plt.imshow(Us,origin='center')
+plt.show()
+
+plt.imshow(Us,interpolation='Gaussian',origin='center',extent=[-1,1,-1,1])
+
+
