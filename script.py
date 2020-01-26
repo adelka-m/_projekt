7103836411c568d2348825a6e3271f89621bdd3e
adelka-m <adelka-m@users.noreply.github.com>
2020-01-26 18:08:09 +0100

    my code, refined only around two points at kink

--- script.py
+++ script.py
@@ -40,6 +40,7 @@ def adapted_grid_CG(n):
 	res = np.zeros((n,n))
 	p = np.zeros((n,n))
 
+
 	h1 = 2/(2*n)
 	h2 = (2-2/n)/(n-2)
 	for i in range(1,(n)//2):
@@ -48,9 +49,7 @@ def adapted_grid_CG(n):
 			res[n-1-i][j] = 1
 			res[i][n-1-j] = 1
 
-    # print('kink point at ', n//2)
-	print(res)
-    # print(p.size)
+
 	p = res.copy()
 	resold = scalar_multiplication(res,res,n)
 	resnew = 0
@@ -58,43 +57,51 @@ def adapted_grid_CG(n):
 	k=1
 	while True:
 
-		refined_grid = {(0,0),(0,1),(1,0),(-1,-1),(-1,0),(0,-1),(1,-1),(-1,1)}
+		refined_grid = {(0,0),(0,1),(1,0),(-1,-1),(-1,0),(0,-1),(1,-1),(-1,1), (2,-1), (2,0), (0,2),(-1,2)}
 		for i,j in refined_grid:
 			i -= 1
 			j -= 1
+			
 			Ap[(n)//2+i][(n)//2+j] = 1/h1**2 * (4*p[(n)//2 +i][(n)//2 +j]  - (p[(n)//2 + i+1][(n)//2 +j]+p[ (n)//2 + i-1][(n)//2 +j]+p[(n)//2 + i][(n)//2 + j+1]+p[(n)//2 +i][(n)//2 +j-1]))
+			
 		for i in range(1,(n)//2):
 			for j in range(1,(n)//2):
-				if i > n//2 - 2 and ((i+1,j+1) not in refined_grid):
-					if i ==5 and j==4:
-						print('juhuu')
-					Ap[i][j] = (2/h1**2 + 2/h2**2) * p[i][j] - (1/h1**2)*(p[i+1][j]+p[i-1][j]) - (1/h2**2)*(p[i][j+1]+p[i][j-1])
-					#print('point ', i,j,'p i j', p[i][j], 'ap i j', Ap[i][j])
+				if i < n//2 -2 and j < n//2 -2:  # region 1
 
+					Ap[i][j]     = 1/h2 **2 * (4*p[i][j] -     (p[i+1][j]      +p[i-1][j]      +p[i][j+1]      +p[i][j-1]))
+					Ap[n-1-i][j] = 1/h2 **2 * (4*p[n-1-i][j] - (p[n-1-(i+1)][j]+p[n-1-(i-1)][j]+p[n-1-i][j+1]  +p[n-1-i][j-1]))
+					Ap[i][n-1-j] = 1/h2 **2 * (4*p[i][n-1-j] - (p[i+1][n-1-j]  +p[i-1][n-1-j]  +p[i][n-1-(j+1)]+p[i][n-1-(j-1)]))
+
+
+				elif  i < n//2 -2 and j > n//2 -3: # region 2
+					# print('nnn:', i,j)
+					# print('nnn1:', n//2 + i+1, j)
+					#print('nn2', n-i-1, j)
+					a = n//2 + i +1
+					b = n-i-1
+					Ap[i][j] = (2/h1**2 + 2/h2**2) * p[i][j]  -   (1/h2**2)*(p[i+1][j]+p[i-1][j])      -(1/h1**2) * (p[i][j+1]  +p[i][j-1]) 
+					Ap[a][j] = (2/h1**2 + 2/h2**2) * p[a][j]  -   (1/h2**2)*(p[a+1][j]+p[a-1][j]) -     (1/h1**2) * (p[a][j+1]  +p[a][j-1]) 
+					Ap[b][j] = (2/h1**2 + 2/h2**2) * p[b][j] -    (1/h2**2)*(p[b+1][j]+p[b-1][j])     - (1/h1**2) * (p[b][(j+1)]+p[b][(j-1)])
+
+					Ap[i][n-j-1] = (2/h1**2 + 2/h2**2) * p[i][n-j-1]  -   (1/h2**2)*(p[i+1][n-j-1]+p[i-1][n-j-1])      -(1/h1**2) * (p[i][n-j-1+1]  +p[i][n-j-1-1])
+
+				elif  j < n//2 -2 and i > n//2 -3:    # region 4
+					# print('AAAA:', i,j)
+					# print('AAAAAAA:', i, n//2+j+1)
+				 # 	print('AAAA!:', n-i-1, j)
+					a = n//2 +j +1
+					Ap[i][j] =     (2/h1**2 + 2/h2**2) * p[i][j]   - (1/h1**2)*(p[i+1][j]      +p[i-1][j])       - (1/h2**2)*(p[i][j+1]+p[i][j-1])
 					Ap[n-1-i][j] = (2/h1**2 + 2/h2**2)*p[n-1-i][j] - (1/h1**2)*(p[n-1-(i+1)][j]+p[n-1-(i-1)][j]) - (1/h2**2)*(p[n-1-i][j+1]+p[n-1-i][j-1]) 
-					#print('point ', n-i,j,'p n-i j', p[n-i][j], 'ap n-i j', Ap[n-i][j])
+					Ap[i][a] =     (2/h1**2 + 2/h2**2)*p[i][a]     - (1/h1**2)*(p[i+1][a]      +p[i-1][a])       - (1/h2**2)*(p[i][a+1]+p[i][a-1])
 
-					Ap[i][n-1-j] = (2/h1**2 + 2/h2**2)*p[i][n-1-j] - (1/h1**2)*(p[i+1][n-1-j]+p[i-1][n-1-j])  - (1/h2**2)*(p[i][n-1-(j+1)]+p[i][n-1-(j-1)])
-					
-				elif j > n//2 - 2 and ((i+1,j+1) not in refined_grid):
-					if i ==5 and j==4:
-						print('juhuu2')
-					Ap[i][j] = (2/h1**2 + 2/h2**2) * p[i][j]  -        (1/h2**2)*(p[i+1][j]+p[i-1][j])      -        (1/h1**2) * (p[i][j+1]+p[i][j-1]) 
-					Ap[n-1-i][j] = (2/h1**2 + 2/h2**2) * p[n-1-i][j] - (1/h2**2)*(p[n-1-(i+1)][j]+p[n-1-(i-1)][j]) - (1/h1**2) * (p[n-1-i][j+1]+p[n-1-i][j-1]) 
-					Ap[i][n-1-j] = (2/h1**2 + 2/h2**2) * p[i][n-1-j] - (1/h2**2)*(p[i+1][n-1-j]+p[i-1][n-1-j])     - (1/h1**2) * (p[i][n-1-(j+1)]+p[i][n-1-(j-1)])
-
-				elif ((i+1,j+1) not in refined_grid): 
-					Ap[i][j] = 1/h2 **2 * (4*p[i][j] - (p[i+1][j]+p[i-1][j]+p[i][j+1]+p[i][j-1]))
-					Ap[n-1-i][j] = 1/h2 **2 * (4*p[n-1-i][j] - (p[n-1-(i+1)][j]+p[n-1-(i-1)][j]+p[n-1-i][j+1]+p[n-1-i][j-1]))
-					Ap[i][n-1-j] = 1/h2 **2 * (4*p[i][n-1-j] - (p[i+1][n-1-j]+p[i-1][n-1-j]+p[i][n-1-(j+1)]+p[i][n-1-(j-1)]))
-    	
+		
 		
 		alpha = resold / scalar_multiplication(p,Ap,n)
 		u = u + alpha * p
 		res = res - alpha * Ap
 
 		derivative = derivative_approx(u[n//2][n//2],u[n//2-1][n//2-1], math.sqrt(2)*h1)
-		print(derivative)
+		
 		resnew = scalar_multiplication(res,res,n)
 
 		#print(resnew)
@@ -103,17 +110,16 @@ def adapted_grid_CG(n):
 			#print( "number of steps: ", k, ", for n = ",n )
 			#break
     	
-		if k > 10:
+		if k > n:
 			break
 		
-		# plt.imshow(u, origin = 'center')
-		# plt.show()
 
 		k = k+1
 		p = res + (resnew / resold) * p
 		resold = resnew
 
-	return u
+
+	return np.sqrt(1/(0.75*n**2)*resnew)
 
 
 def conjugated_gradients(n):
@@ -222,22 +228,18 @@ def conjugated_gradients_square(n):
     return math.sqrt((1/n**2)*resnew)
 
 
-u = adapted_grid_CG(2*300)
 
-plt.imshow(u, origin = 'center')
-plt.show()
-# res = []
+
+res = []
 # res_square = []
-# N = np.arange(100,156,10)
-# for n in N:
-# 	print(n)
-# 	res.append( math.sqrt(conjugated_gradients(2*n)/(3*(n-1)**2)) )
-# 	res_square.append(conjugated_gradients_square(2*n))
+N = np.arange(10,96,20)
+for n in N:
+	print(n)
+	res.append(adapted_grid_CG(n))
 
-# alpha = np.log(res[-1]/res[-2])/np.log((3*(N[-1]-1)**2)/((3*(N[-2]-1)**2)))
-# print(alpha)
-# alpha = np.log(res_square[-1]/res_square[-2])/np.log((3*(N[-1]-1)**2)/((3*(N[-2]-1)**2)))
-# print(alpha)
+
+
+
 # xx = []
 # for n in N:
 # 	xx.append(  ((3*(n-1)**2))**alpha * res[0] * ((3*(N[0]-1)**2))**(-alpha) )
@@ -250,14 +252,14 @@ plt.show()
 
 # print('slope L:', slope_L,', slope square:', slope_square)
 # #plt.semilogy( 2*N+1, xx , linestyle='dashed', label = 'order of -1/2')
-# plt.semilogy( 2*N+1, res, linestyle='dotted',marker='o',label = 'discrete L2 norm of residuum')
+plt.semilogy(N, res, linestyle='dotted',marker='o')
 # plt.semilogy( 2*N+1, res_square, linestyle='dotted',marker='*',label = 'discrete L2 norm of residuum square')
 
 # plt.title("Value of residual error")
 # plt.xlabel("Number of points") 
 # plt.ylabel("Error (log scale)")
 # plt.legend()
-# plt.show()
+plt.show()
 # plt.imshow(res[0], interpolation = 'lanczos', origin = 'center', extent = [-1,1,-1,1])
 # plt.colorbar()
 # plt.show()
